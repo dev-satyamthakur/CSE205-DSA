9ad5cb22471e8c993f7eadd3289d45a22cedd837
Neo <66131727+dev-satyamthakur@users.noreply.github.com>
2022-09-30 22:33:36 +0530

    Add files via upload

---
+++ INT213-Python-Programming/K21RN-Contest19/Restaurant.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'restaurant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER l
+#  2. INTEGER b
+#
+
+def calculate_hcf(x, y):  
+    
+    if x > y:  
+        smaller = y  
+    else:  
+        smaller = x  
+    for i in range(1,smaller + 1):  
+        if((x % i == 0) and (y % i == 0)):  
+            hcf = i  
+    return hcf  
+def restaurant(l, b):
+    x=calculate_hcf(l,b)
+    return int(l*b/(x*x))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        l = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = restaurant(l, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
