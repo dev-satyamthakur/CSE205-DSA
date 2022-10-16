4a272a6be3d89ce809b41f03e1e3848d5e4bcfc7
Neo <66131727+dev-satyamthakur@users.noreply.github.com>
2022-10-16 11:38:27 +0530

    Update Queue-2-21482.cpp

--- CSE205-DSA/CSE205-28845-DAY28/Queue-2-21482.cpp
+++ CSE205-DSA/CSE205-28845-DAY28/Queue-2-21482.cpp
@@ -1,50 +1,38 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    stack<int> s1, s2;
-    int num_operations;
-    cin>>num_operations;
-    int Q_operation, x;
-    for(int i=0; i<num_operations; i++){
-        cin>>Q_operation;
-        if(Q_operation == 1){
-            cin>>x;
-            s1.push(x);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    queue<int> q;
+    int n, k;
+    cin >> n >> k;
+    
+    for (int i = 0; i < n; ++i) {
+        int l;
+        cin >> l;
+        q.push(l);
+    }
+    
+    for (int i = 0; i < k; ++i) {
+        int l;
+        cin >> l;
+        
+        if (l == 1) {
+            int ele = q.front();
+            q.pop();
+            q.push(ele);
         }
-        if(Q_operation == 2){
-            if(!s2.empty()){
-                s2.pop();
-            }
-            else{
-                while(!s1.empty()){
-                    s2.push(s1.top());
-                    s1.pop();
-                }
-                s2.pop();
-            }
-        }
-        if(Q_operation == 3){
-
-            if(!s2.empty()){
-            cout<<s2.top()<<endl;
-            }
-            else{
-                while(!s1.empty()){
-                    s2.push(s1.top());
-                    s1.pop();
-                }
-                cout<<s2.top()<<endl;
-            }
+        else if (l == 0) {
+            q.pop();
         }
     }
+    
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
     
     return 0;
 }
